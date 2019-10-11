5ab9d9d83e2274e27669d13bdf887312b0e1e6f5
Manuel de la Peña <mdelapenya@gmail.com>
2019-10-11 14:14:12 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/mdelapenya/savage-adventure-cards
+
+go 1.12
+
+require (
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/flopp/go-findfont v0.0.0-20190424081857-5592ff8eef0e
+	github.com/fogleman/gg v1.3.0
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/manifoldco/promptui v0.3.2
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+	gopkg.in/alecthomas/kingpin.v3-unstable v3.0.0-20180810215634-df19058c872c // indirect
+)
---
+++ main.go
@@ -0,0 +1,392 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/flopp/go-findfont"
+	"github.com/fogleman/gg"
+	"github.com/golang/freetype/truetype"
+	"github.com/manifoldco/promptui"
+)
+
+type imageDimensions struct {
+	x int
+	y int
+}
+
+var basePath = "."
+
+const defaultCardsPath = "cards"
+const defaultImagesPath = "images"
+const defaultBodyFont = "Arial"
+const defaultHeaderFont = "Adventure"
+const defaultTitleFont = "Adventure"
+const defaultOutputsPath = "outputs"
+const defaultTemplatesPath = "templates"
+const defaultTextsPath = "texts"
+
+var cardsDims = imageDimensions{
+	x: 243, y: 340,
+}
+var imageDims = imageDimensions{
+	x: 196, y: 157,
+}
+
+type cardConfig struct {
+	BodyFont    string
+	HeaderFont  string
+	ImagesPath  string
+	OutputsPath string
+	Template    string
+	TextsPath   string
+	TitleFont   string
+}
+
+func main() {
+	basePath = promptBasePath()
+
+	imagesPath := promptPath("images directory", path.Join(basePath, defaultImagesPath))
+	validateImages(imagesPath, imageDims)
+
+	templatesPath := promptPath("templates directory", path.Join(basePath, defaultTemplatesPath))
+	validateImages(templatesPath, cardsDims)
+	template := promptTemplate(templatesPath)
+
+	textsPath := promptPath("texts directory", path.Join(basePath, defaultTextsPath))
+	outputsPath := promptPath("output directory", path.Join(basePath, defaultOutputsPath))
+
+	hf := promptTrueTypeFonts("Header")
+	tf := promptTrueTypeFonts("Title")
+	bf := promptTrueTypeFonts("Body")
+
+	cardCfg := cardConfig{
+		HeaderFont:  hf,
+		TitleFont:   tf,
+		BodyFont:    bf,
+		Template:    path.Join(templatesPath, template),
+		OutputsPath: outputsPath,
+		ImagesPath:  imagesPath,
+		TextsPath:   textsPath,
+	}
+
+	processImages(cardCfg)
+	validateImages(outputsPath, cardsDims)
+}
+
+func addText(source image.Image, f string, text string, fontSize float64, offset float64, output string) {
+	_, fontPath := getTrueTypeFont(f)
+
+	x := source.Bounds().Max.X
+	y := source.Bounds().Max.Y
+
+	dc := gg.NewContext(x, y)
+	dc.AsMask()
+	dc.Clear()
+	dc.SetRGB(0, 0, 0)
+	if err := dc.LoadFontFace(fontPath, fontSize); err != nil {
+		log.Fatalf("Cannot load font from %s: %v", fontPath, err)
+	}
+
+	dc.DrawStringAnchored(text, 21, 188, 0.5, 0.5)
+	dc.DrawRoundedRectangle(0, 0, float64(x), float64(y), 0)
+	dc.DrawImage(source, 0, 0)
+	dc.DrawStringWrapped(text, float64(x/2), float64(y/2)+offset, 0.5, 0.5, float64(x-10), 1.0, gg.AlignCenter)
+	dc.Clip()
+	dc.SavePNG(output)
+}
+
+func decodeFileAsPng(f *os.File) image.Image {
+	pngImage, err := png.Decode(f)
+	if err != nil {
+		log.Fatalf("Failed to decode: %s", err)
+	}
+
+	return pngImage
+}
+
+func getFileName(f string) string {
+	name := filepath.Base(f)
+	extension := filepath.Ext(f)
+
+	return name[0 : len(name)-len(extension)]
+}
+
+// getTrueTypeFonts
+func getTrueTypeFonts() []string {
+	fonts := findfont.List()
+
+	fontNames := []string{}
+	for _, font := range fonts {
+		fontNames = append(fontNames, getFileName(font))
+	}
+
+	return fontNames
+}
+
+func getTrueTypeFont(fontName string) (*truetype.Font, string) {
+	fontPath, err := findfont.Find(fontName)
+	if err != nil {
+		log.Fatalf("Could not find %s font in the System: %v", fontName, err)
+	}
+	log.Printf("Found '%s' in '%s'\n", fontName, fontPath)
+
+	// load the font with the freetype library
+	fontData, err := ioutil.ReadFile(fontPath)
+	if err != nil {
+		log.Fatalf("Could not read %s font: %v", fontName, err)
+	}
+	font, err := truetype.Parse(fontData)
+	if err != nil {
+		log.Fatalf("Could not parse truetype %s font: %v", fontName, err)
+	}
+
+	return font, fontPath
+}
+
+func openFile(f string) *os.File {
+	file, err := os.Open(f)
+	if err != nil {
+		log.Fatalf("Failed to open: %s", err)
+	}
+
+	return file
+}
+
+func pathExists(p string) error {
+	if _, err := os.Stat(p); os.IsNotExist(err) {
+		return errors.New("Path does not exist")
+	}
+
+	return nil
+}
+
+func processImage(cfg cardConfig, imagePath string) {
+	templateFile := openFile(cfg.Template)
+	templatePng := decodeFileAsPng(templateFile)
+	defer templateFile.Close()
+
+	imageFile := openFile(imagePath)
+	imagePng := decodeFileAsPng(imageFile)
+	defer imageFile.Close()
+
+	offset := image.Pt(22, 22)
+	bounds := templatePng.Bounds()
+
+	cardPng := image.NewRGBA(bounds)
+
+	draw.Draw(cardPng, bounds, templatePng, image.ZP, draw.Src)
+	draw.Draw(cardPng, imagePng.Bounds().Add(offset), imagePng, image.ZP, draw.Over)
+
+	imageName := getFileName(imagePath)
+
+	outputFilePath := path.Join(cfg.OutputsPath, "card_"+imageName+".png")
+
+	cardFile, err := os.Create(outputFilePath)
+	if err != nil {
+		log.Fatalf("failed to create: %s", err)
+	}
+
+	pngEnconder := png.Encoder{
+		CompressionLevel: png.BestCompression,
+	}
+	pngEnconder.Encode(cardFile, cardPng)
+	defer cardFile.Close()
+
+	textFilePath := path.Join(cfg.TextsPath, imageName+".txt")
+	texts := readTextFile(textFilePath)
+
+	addText(cardPng, cfg.HeaderFont, texts["header"], 14, 20, outputFilePath)
+	outputFile := openFile(outputFilePath)
+	outputPng := decodeFileAsPng(outputFile)
+
+	addText(outputPng, cfg.TitleFont, texts["title"], 12, 60, outputFilePath)
+	outputFile = openFile(outputFilePath)
+	outputPng = decodeFileAsPng(outputFile)
+
+	addText(outputPng, cfg.BodyFont, texts["body"], 10, 100, outputFilePath)
+	outputFile = openFile(outputFilePath)
+	outputPng = decodeFileAsPng(outputFile)
+}
+
+func processImages(cfg cardConfig) {
+	images, _ := ioutil.ReadDir(cfg.ImagesPath)
+	for _, img := range images {
+		imgPath := path.Join(cfg.ImagesPath, img.Name())
+
+		processImage(cfg, imgPath)
+	}
+}
+
+func promptPath(label string, defaultValue string) string {
+	prompt := promptui.Prompt{
+		Label:    "Location of the " + label + ": ",
+		Default:  defaultValue,
+		Validate: pathExists,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		log.Fatalf("Prompt failed %v\n", err)
+	}
+
+	fmt.Printf("You choose %q\n", result)
+
+	return result
+}
+
+func promptBasePath() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatalf("Cannot find current user %v", err)
+	}
+
+	return promptPath("root directory", usr.HomeDir)
+}
+
+func promptImagesPath() string {
+	return promptPath("images directory", path.Join(basePath, defaultImagesPath))
+}
+
+func promptTemplate(templatesPath string) string {
+	items := []string{}
+
+	files, _ := ioutil.ReadDir(templatesPath)
+	for _, templateFile := range files {
+		if !strings.HasSuffix(templateFile.Name(), ".png") {
+			continue
+		}
+
+		items = append(items, templateFile.Name())
+	}
+	if len(items) == 0 {
+		log.Fatalf("There are no templates in %s", templatesPath)
+	}
+
+	prompt := promptui.Select{
+		Label: "Select Template",
+		Items: items,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		log.Fatalf("Prompt failed %v\n", err)
+	}
+
+	fmt.Printf("You choose %q\n", result)
+
+	return result
+}
+
+func promptTrueTypeFonts(fontType string) string {
+	fonts := getTrueTypeFonts()
+
+	if len(fonts) == 0 {
+		log.Fatal("There are no TrueType fonts in the System")
+	}
+
+	prompt := promptui.Select{
+		Label: "Select " + fontType + " Font",
+		Items: fonts,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		log.Fatalf("Prompt failed %v\n", err)
+	}
+
+	fmt.Printf("You choose %q\n", result)
+
+	return result
+}
+
+type cardText map[string]string
+
+func readTextFile(filename string) cardText {
+	card := cardText{
+		"header": "HEADER",
+		"title":  "TITLE",
+		"body":   "BODY",
+	}
+	if len(filename) == 0 {
+		log.Fatal("Text file is empty")
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal("Cannot open text file: %v", err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadString('\n')
+
+		// check if the line has = sign
+		// and process the line. Ignore the rest.
+		if equal := strings.Index(line, "="); equal >= 0 {
+			if key := strings.TrimSpace(line[:equal]); len(key) > 0 {
+				value := ""
+				if len(line) > equal {
+					value = strings.TrimSpace(line[equal+1:])
+				}
+				// assign the config map
+				card[key] = value
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal("Cannot read text file: %v", err)
+		}
+	}
+	return card
+}
+
+func validateImages(subDir string, dims imageDimensions) {
+	dir := path.Join(basePath, subDir)
+
+	files, _ := ioutil.ReadDir(dir)
+
+	for _, imgFile := range files {
+		if !strings.HasSuffix(imgFile.Name(), ".png") {
+			log.Fatalf("All images in '%s' must be valid PNG files: %s", subDir, imgFile.Name())
+		}
+
+		if reader, err := os.Open(filepath.Join(dir, imgFile.Name())); err == nil {
+			defer reader.Close()
+
+			im, _, err := image.DecodeConfig(reader)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %v\n", imgFile.Name(), err)
+				continue
+			}
+
+			if im.Width != dims.x && im.Height != dims.y {
+				log.Fatalf("%s does not match dimensions (%d x %d pixels): %v", imgFile.Name(), dims.x, dims.y, err)
+			}
+
+		} else {
+			log.Fatalf("Impossible to open the file: %v", err)
+		}
+	}
+
+	log.Printf("All images in '%s' are PNG and satisfy dims (%d x %d px)", subDir, dims.x, dims.y)
+}
